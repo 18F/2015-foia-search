981715778dfbedf900d64dd6cfcf074da5a14700
Eric Mill <eric.mill@gsa.gov>
2014-07-18 18:33:37 -0400

    Initial commit, hello world app.

---
+++ app.js
@@ -0,0 +1,33 @@
+var routes = require("./routes");
+
+var express = require('express');
+var app = express();
+
+// environment and port
+var env = process.env.NODE_ENV || 'development';
+var port = parseInt(process.argv[2], 10);
+if (isNaN(port)) port = 3000;
+
+// app middleware/settings
+app.engine('.html', require('ejs').__express);
+app.enable('trust proxy')
+  .use(require('body-parser').json())
+  .use(require('body-parser').urlencoded({extended: false}))
+  .use(require('method-override')())
+  .use(express.static(__dirname + '/public'));
+
+// development vs production
+if (env == "development")
+  app.use(require('errorhandler')({dumpExceptions: true, showStack: true}))
+else
+  app.use(require('errorhandler')())
+
+
+// helpers and routes
+app.get('/', routes.index);
+
+
+// boot it up!
+app.listen(port, function() {
+  console.log("Express server listening on port %s in %s mode", port, env);
+});
